75698191200042fd0db5208281071696cc3b6a08
sviatoslav <b.svjatoslav@gmail.com>
2024-04-10 22:49:00 +0300

    Added transforms to plain

--- mangoPURE/data/plain.py
+++ mangoPURE/data/plain.py
@@ -1,16 +1,18 @@
 from datasets import Dataset as HfDataset
 from torchaudio.transforms import Resample
 from .mixer import MixedExample
-from .base import SegmentInfo
+from .base import SegmentInfo, MixerTransform
 from .utils import Name2IdMapper
 from ..globals import RATE
+from typing import List
 
 
 class AudioDataset:
-    def __init__(self, dataset: HfDataset):
+    def __init__(self, dataset: HfDataset, transforms: List[MixerTransform] = None):
         self.dataset = dataset
         self.idx = 0
         self.len = len(dataset)
+        self.transforms = transforms
         self.mapper = Name2IdMapper(start_id=0)
 
     def generate(self) -> MixedExample:
@@ -21,4 +23,7 @@ class AudioDataset:
         self.idx += 1
         if self.idx >= self.len:
             self.idx = 0
-        return MixedExample(audio=audio, noises_info=[SegmentInfo(class_id=noise, start=0.0, end=1.0)])
+        example = MixedExample(audio=audio, noises_info=[SegmentInfo(class_id=noise, start=0.0, end=1.0)])
+        for transform in self.transforms:
+            example = transform(example)
+        return example
